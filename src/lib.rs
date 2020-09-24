03861439e56a44a4a89ef5592b6cb1a1255e561d
sokomishalov <sokomishalov@mail.ru>
2020-09-24 15:06:59 +0300

    further progress

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,22 @@
 extern crate proc_macro;
 
-use proc_macro::TokenStream;
+use proc_macro::TokenStream as TokenStream;
+
+use proc_macro2::TokenStream as TokenStream2;
+use quote::quote;
+use syn::{DeriveInput, parse_macro_input};
 
 #[proc_macro_derive(Getter)]
-pub fn derive_getter(_item: TokenStream) -> TokenStream {
-    "".parse().unwrap()
+pub fn derive_getter(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+
+    let name = input.ident;
+
+    let expanded: TokenStream2 = quote! {
+        impl #name {
+            // todo
+        }
+    };
+
+    return TokenStream::from(expanded);
 }
