93828e146f65cabc6cc6e248eba3b20705f0ed5d
sokomishalov <sokomishalov@mail.ru>
2020-10-02 14:21:09 +0300

    no_args_constructor started

--- src/no_args_constructor.rs
+++ src/no_args_constructor.rs
@@ -2,14 +2,36 @@ use proc_macro::TokenStream as TokenStream;
 
 use proc_macro2::TokenStream as TokenStream2;
 use quote::quote;
-use syn::DeriveInput;
+use syn::{Data, DataStruct, DeriveInput, Fields};
 
 pub(crate) fn no_args_constructor(input: DeriveInput) -> TokenStream {
     let name = input.ident.clone();
+    let body = generate_body(input);
 
     TokenStream::from(quote! {
         impl #name {
+            #body
+        }
+    })
+}
 
+fn generate_body(input: DeriveInput) -> TokenStream2 {
+    let fields = match &input.data {
+        Data::Struct(DataStruct { fields: Fields::Named(fields), .. }) => &fields.named,
+        _ => return TokenStream2::new(),
+    };
+    let structure_params = fields
+        .iter()
+        .map(|field| {
+            let field_name = field.ident.clone().unwrap();
+            quote! {
+                #field_name: ::core::default::Default::default(),
+            }
+        });
+
+    TokenStream2::from(quote! {
+        pub fn new() -> Self {
+            Self {#(#structure_params)*}
         }
     })
 }
