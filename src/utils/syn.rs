6b4f93e96b513747fd2f9c26f8ec415ae96a95de
sokomishalov <sokomishalov@mail.ru>
2020-10-02 20:45:25 +0300

    refactoring

--- src/utils/syn.rs
+++ src/utils/syn.rs
@@ -18,6 +18,15 @@ pub(crate) fn parse_derive_input(input: TokenStream) -> DeriveInput {
 pub(crate) fn named_fields(input: &DeriveInput) -> &Punctuated<Field, Comma> {
     match &input.data {
         Data::Struct(DataStruct { fields: Fields::Named(fields), .. }) => &fields.named,
-        _ => panic!("There is no named fields "),
+        _ => panic!("This struct does not contain named fields!"),
+    }
+}
+
+#[allow(dead_code)]
+pub(crate) fn all_fields(input: &DeriveInput) -> &Punctuated<Field, Comma> {
+    match &input.data {
+        Data::Struct(DataStruct { fields: Fields::Named(fields), .. }) => &fields.named,
+        Data::Struct(DataStruct { fields: Fields::Unnamed(fields), .. }) => &fields.unnamed,
+        _ => panic!("This struct does not contain any fields!"),
     }
 }
