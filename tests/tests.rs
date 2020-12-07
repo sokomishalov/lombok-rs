ee3666b5e26f1a502cbe89f313b7ab7c9c466f74
sokomishalov <sokomishalov@mail.ru>
2020-12-07 19:36:09 +0300

    setters test

--- tests/tests.rs
+++ tests/tests.rs
@@ -6,9 +6,7 @@
 extern crate lombok_derive;
 extern crate lombok_trait;
 
-#[derive(
-    Getter, Setter, AllArgsConstructor, NoArgsConstructor, Builder, EqualsAndHashcode, ToString,
-)]
+#[derive(Getter, Setter, NoArgsConstructor, Builder, EqualsAndHashcode, ToString, Clone)]
 pub struct TestNamedStructure<'a> {
     age: u8,
     nick: &'a str,
@@ -16,23 +14,49 @@ pub struct TestNamedStructure<'a> {
     hobby: Box<String>,
 }
 
-#[cfg(test)]
-#[allow(dead_code)]
-mod tests {
-    use super::TestNamedStructure;
-
-    #[test]
-    fn test_getters() {
-        let data = TestNamedStructure {
+impl Default for TestNamedStructure<'_> {
+    fn default() -> Self {
+        TestNamedStructure {
             age: 25,
             nick: "sokomishalov",
             languages: vec!["rust".to_string(), "kotlin".to_string()],
             hobby: Box::new("soccer".to_string()),
-        };
+        }
+    }
+}
 
-        assert_eq!(&data.age, data.get_age());
-        assert_eq!(&data.nick, &data.get_nick());
-        assert_eq!(&data.languages, data.get_languages());
-        assert_eq!(&data.hobby, data.get_hobby());
+#[cfg(test)]
+#[allow(dead_code)]
+mod tests {
+    use lombok_trait::NoArgsContructor;
+
+    use super::TestNamedStructure;
+
+    #[test]
+    fn test_getters() {
+        let default = TestNamedStructure::default();
+
+        assert_eq!(&default.age, default.get_age());
+        assert_eq!(default.nick, default.get_nick());
+        assert_eq!(&default.languages, default.get_languages());
+        assert_eq!(&default.hobby, default.get_hobby());
+    }
+
+    #[test]
+    fn test_setters() {
+        let mut data = TestNamedStructure::new();
+
+        let default = TestNamedStructure::default();
+        let clone_default = default.clone();
+
+        data.set_age(clone_default.age);
+        data.set_nick(clone_default.nick);
+        data.set_languages(clone_default.languages);
+        data.set_hobby(clone_default.hobby);
+
+        assert_eq!(default.age, data.age);
+        assert_eq!(default.nick, data.nick);
+        assert_eq!(default.languages, data.languages);
+        assert_eq!(default.hobby, data.hobby);
     }
 }
